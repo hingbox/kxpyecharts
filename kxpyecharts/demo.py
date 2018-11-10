7617b1cf8866cb54f6138dad796b438e6b29492c
jasonkuangs <javaspring_kuang@163.com>
2018-11-10 13:40:14 +0800

    初始提交 demo

---
+++ kxpyecharts/demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#encoding=utf-8
+'''
+@描述：PyCharm
+@作者：hingbox
+@邮箱：hingbox@163.com
+@版本：V1.0
+@文件名称 : demo.py
+@创建时间：2018/11/10 13:34
+'''
+from __future__ import unicode_literals
+from pyecharts import Bar
+bar = Bar("我的第一个图表", "这里是副标题")
+bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
+# bar.print_echarts_options() # 该行只为了打印配置项，方便调试时使用
+bar.render()    # 生成本地 HTML 文件
